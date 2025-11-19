23062a340df1fa089f752a36597d04948d6ec7bd
Alejandro <a.guzmanr2@uniandes.edu.co>
2025-11-19 08:41:35 -0500

    1 y 2 ?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmprendedorDetailComponent } from './emprendedor/emprendedor-detail/emprendedor-detail.component'; 
 import { EmprendedorListComponent } from './emprendedor/emprendedor-list/emprendedor-list.component';
+import { EmprendedorDetailComponent } from './emprendedor/emprendedor-detail/emprendedor-detail.component';
 
 const routes: Routes = [
-  {
-    path: 'emprendedores',
-    component: EmprendedorListComponent
-  },
-  {
-    path: 'emprendedores/:id',
-    component: EmprendedorDetailComponent
-  },
-  {
-    path: '',
-    redirectTo: 'emprendedores',
-    pathMatch: 'full'
-  }
+  { path: 'emprendedores', component: EmprendedorListComponent },
+  { path: 'emprendedores/:id', component: EmprendedorDetailComponent },
+  { path: '', redirectTo: '/emprendedores', pathMatch: 'full' }
 ];
 
 @NgModule({
